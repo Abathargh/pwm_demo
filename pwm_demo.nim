48dc720a56ca3326999b4ea340561cedfefb3595
mar <gim.marcello@gmail.com>
2024-11-11 18:45:57 +0100

    added: base project with a couple of dt/freqs and navigation.

---
+++ pwm_demo.nim
@@ -0,0 +1,99 @@
+import std/macros
+import std/tables
+import avr_io
+
+
+const
+  buttonPin = 0 # PIN8 => PORTB[0]
+  tim2Out   = 1 # PIN3 => PORTD[3]
+
+
+type PwmState = enum
+  Pwm50DT1Hz
+  Pwm20DT1Hz
+  Pwm80DT1Hz
+  Pwm50DT10Hz
+  Pwm50DT50Hz
+  Pwm20DT50Hz
+  Pwm80DT50Hz
+
+
+proc delayMs(ms: uint16) {.importc: "_delay_ms", header: "util/delay.h"}
+
+
+proc readDebounced(p: Port, pin: uint8): uint8 =
+  const debounceResolution = 8
+  var   accumulator = 0'u8
+
+  while true:
+    var counter = 0
+    while counter < debounceResolution:
+      accumulator = (accumulator shl 1) or (portB.readPin buttonPin)
+      delayMs 1
+      inc counter
+    if accumulator == 0x00 or accumulator == 0xff:
+     return accumulator
+
+
+template setPwm(timer: Timer, freq: uint32, dt: uint8) =
+  const
+    mcuFreq = 16e6.uint32
+    pwmPre  = 1024.uint32 # No prescaler in use
+    ocraVal  = mcuFreq div (pwmPre * freq) - 1
+    ocrbVal  = (ocraVal * dt) div 100
+
+  timer1.ocra[] = ocraVal.uint16
+  timer1.ocrb[] = ocrbVal.uint16
+
+
+proc initPwmTimer1 =
+  # 16-bit timer1 in CTC mode using interrupts
+  timer1.setTimerFlag({TimCtlB16Flag.wgm2, cs0, cs2})
+  timer1.setPwm(1, 50)
+  timer1.setTimerFlag({Timsk16Flag.ociea, ocieb})
+
+
+proc setPwmHandle   {.isr(Timer1CompAVect).} = portB.setPin(tim2Out)
+proc clearPwmHandle {.isr(Timer1CompBVect).} = portB.clearPin(tim2Out)
+
+
+proc loop =
+  portB.asOutputPin(tim2Out)
+  portB.asInputPullupPin(buttonPin)
+  initPwmTimer1()
+
+  sei()
+
+  var
+    state = Pwm50DT1Hz
+    buttonState = false
+
+  while true:
+    let pressed = portB.readDebounced(buttonPin) == 0x00
+
+    if pressed and pressed != buttonState:
+      if state == PwmState.high:
+        state = PwmState.low
+      else:
+        inc state
+
+      case state
+      of Pwm50DT1Hz:
+        timer1.setPwm(1, 50)
+      of Pwm20DT1Hz:
+        timer1.setPwm(1, 20)
+      of Pwm80DT1Hz:
+        timer1.setPwm(1, 80)
+      of Pwm50DT10Hz:
+        timer1.setPwm(10, 50)
+      of Pwm50DT50Hz:
+        timer1.setPwm(50, 50)
+      of Pwm20DT50Hz:
+        timer1.setPwm(50, 20)
+      of Pwm80DT50Hz:
+        timer1.setPwm(50, 80)
+
+    buttonState = pressed
+
+
+loop()
---
+++ pwm_demo.nimble
@@ -0,0 +1,33 @@
+# Package
+
+version       = "0.1.0"
+author        = "mar"
+description   = "A new awesome nimble package"
+license       = "MIT"
+bin           = @["pwm_demo"]
+
+
+# Dependencies
+
+requires "nim >= 2.2.0"
+
+requires "avr_io >= 0.4.0"
+
+after build:
+  when defined(windows):
+    mvFile(bin[0] & ".exe", bin[0] & ".elf")
+  else:
+    mvFile(bin[0], bin[0] & ".elf")
+  exec("avr-objcopy -O ihex " & bin[0] & ".elf " & bin[0] & ".hex")
+  exec("avr-objcopy -O binary " & bin[0] & ".elf " & bin[0] & ".bin")
+
+task clear, "Deletes the previously built compiler artifacts":
+  rmFile(bin[0] & ".elf")
+  rmFile(bin[0] & ".hex")
+  rmFile(bin[0] & ".bin")
+  rmDir(".nimcache")
+task flash, "Loads the compiled binary onto the MCU":
+  exec("avrdude -c arduino -b 115200 -P/dev/ttyACM0 -p atmega328p -U flash:w:" & bin[0] & ".hex:i")
+
+task flash_debug, "Loads the elf binary onto the MCU":
+  exec("avrdude -c arduino -b 115200 -P/dev/ttyACM0 -p atmega328p -U flash:w:" & bin[0] & ".elf:e")
